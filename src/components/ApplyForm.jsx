121e0e39229a5d038de507ada877116042f219bd
woic-ej <woic0102@naver.com>
2024-04-09 20:24:53 +0900

    ApplyForm user변수에 wantedDate 초기값 설정, input email의 유효성 검사를 통해  onBlur핸들러  설정

--- src/components/ApplyForm.jsx
+++ src/components/ApplyForm.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 import "../styles/ApplyForm.css";
 
 const ApplyForm = () => {
-  const [isError, setIsError] = useState(false);
+  const [Error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState({
+    secondName: "",
+    firstName: "",
+    membership: "",
+    email: "",
+    wantedDate: "sat",
+  });
 
   const handleInputChange = (event) => {
     event.preventDefault();
@@ -17,12 +23,38 @@ const ApplyForm = () => {
     });
   };
 
-  const validateEmail = () => {
-    const email = document.getElementById("email");
+  const isValidEmail = () => {
+    const email = user.email;
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!regex.test(email)) {
       alert("올바른 이메일 주소를 입력하십시오");
+      return false;
+    }
+    return true;
+  };
+
+  const handleEmailBlur = () => {
+    isValidEmail();
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!isValidEmail()) return;
+
+    setIsLoading(true);
+    try {
+      const response = await fetch("http://localhost:3001/user", {
+        method: "POST",
+        cache: "no-cache",
+        body: JSON.stringify(user),
+      });
+      if (!response.ok) throw new Error("에러가 발생하였습니다.");
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -40,23 +72,25 @@ const ApplyForm = () => {
       </div>
       <hr></hr>
       <div className="apply-form">
-        <form method="POST" onChange={handleInputChange}>
+        <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="second-name">이름</label>
+            <label htmlFor="secondName">이름</label>
             <input
               type="text"
-              id="second-name"
-              name="second-name"
+              id="secondName"
+              name="secondName"
               required
+              onChange={handleInputChange}
             ></input>
           </div>
           <div>
-            <label htmlFor="first-name">성</label>
+            <label htmlFor="firstName">성</label>
             <input
               type="text"
-              id="first-name"
-              name="first-name"
+              id="firstName"
+              name="firstName"
               required
+              onChange={handleInputChange}
             ></input>
           </div>
           <div>
@@ -66,21 +100,34 @@ const ApplyForm = () => {
               id="membership"
               name="membership"
               required
+              onChange={handleInputChange}
             ></input>
           </div>
           <div>
             <label htmlFor="email">이메일</label>
-            <input type="text" id="email" name="email" required></input>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              required
+              onChange={handleInputChange}
+              onBlur={handleEmailBlur}
+            ></input>
           </div>
           <div>
             <label htmlFor="wantedDate">응모 날짜</label>
-            <select id="wantedDate" name="wantedDate" required>
-              <option value="sat"> 2024.5.11 SAT 6PM </option>
+            <select
+              id="wantedDate"
+              name="wantedDate"
+              required
+              onChange={handleInputChange}
+            >
+              <option value="sat">2024.5.11 SAT 6PM</option>
               <option value="sun"> 2024 5.12 SUN 5PM </option>
             </select>
           </div>
 
-          <button type="submit" className="applyBtn" onClick={validateEmail}>
+          <button type="submit" className="applyBtn">
             응모 하기
           </button>
         </form>
