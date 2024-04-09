b0153eca0384b5fb4cab49ec35e3b01a1ba392cc
woic-ej <woic0102@naver.com>
2024-04-09 17:44:08 +0900

    ApplyForm의onChange핸들러 생성

--- src/components/ApplyForm.jsx
+++ src/components/ApplyForm.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import "../styles/ApplyForm.css";
 
 const ApplyForm = () => {
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState({});
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+
+    setUser({
+      ...user,
+      [inputName]: inputValue,
+    });
+  };
+
   return (
     <div className="form-container">
       <div className="info-text">
@@ -15,26 +31,41 @@ const ApplyForm = () => {
       </div>
       <hr></hr>
       <div className="apply-form">
-        <form method="POST">
+        <form method="POST" onChange={handleInputChange}>
           <div>
             <label htmlFor="second-name">이름</label>
-            <input type="text" id="second-name" name="second-name"></input>
+            <input
+              type="text"
+              id="second-name"
+              name="second-name"
+              required
+            ></input>
           </div>
           <div>
             <label htmlFor="first-name">성</label>
-            <input type="text" id="first-name" name="first-name"></input>
+            <input
+              type="text"
+              id="first-name"
+              name="first-name"
+              required
+            ></input>
           </div>
           <div>
             <label htmlFor="membership">멤버쉽 번호</label>
-            <input type="text" id="membership" name="membership"></input>
+            <input
+              type="text"
+              id="membership"
+              name="membership"
+              required
+            ></input>
           </div>
           <div>
             <label htmlFor="email">이메일</label>
-            <input type="text" id="email" name="email"></input>
+            <input type="text" id="email" name="email" required></input>
           </div>
           <div>
             <label htmlFor="wantedDate">응모 날짜</label>
-            <select id="wantedDate" name="wantedDate">
+            <select id="wantedDate" name="wantedDate" required>
               <option value="sat"> 2024.5.11 SAT 6PM </option>
               <option value="sun"> 2024 5.12 SUN 5PM </option>
             </select>
